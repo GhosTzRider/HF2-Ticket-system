134e87f10dd0b82493014e1028f7bc50e7c35cfb
Marcus Ellekær Rollmann <145582140+GhosTzRider@users.noreply.github.com>
2026-04-20 11:52:19 +0200

    Add files via upload

---
+++ TS_Delete_Script.sql
@@ -0,0 +1,8 @@
+DROP TABLE dbo.tickets;
+DROP TABLE dbo.users;
+DROP TABLE dbo.acticles;
+DROP TABLE dbo.categories;
+DROP TABLE dbo.services;
+DROP TABLE dbo.priorities;
+DROP TABLE dbo.statuses;
+DROP DATABASE Knowledge;
