b38e717c4165d866e433a1d59d61e167b1e061e0
Marcus Rollmann <Marcus@rollmann.dk>
2026-04-20 13:02:18 +0200

    Add new pages and styles for About, Contact, Create Ticket, and Tech Knowledge sections; update navigation links

--- HF2_Ticket_system/TS/urls.py
+++ HF2_Ticket_system/TS/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('create/', views.create_ticket, name='create_ticket'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('tech_knowledge/', views.tech_knowledge, name='tech_knowledge'),
 ]
--- HF2_Ticket_system/TS/views.py
+++ HF2_Ticket_system/TS/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def create_ticket(request):
+    return render(request, 'create_ticket.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def tech_knowledge(request):
+    return render(request, 'tech_knowledge.html')
