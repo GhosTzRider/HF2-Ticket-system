e5cea8cee2d36c8b98e16f0e1c8332269d89b198
Marcus Ellekær Rollmann <145582140+GhosTzRider@users.noreply.github.com>
2026-04-27 08:18:41 +0200

    Delete TS_Delete_Script.sql

--- TS_Delete_Script.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE dbo.tickets;
-DROP TABLE dbo.users;
-DROP TABLE dbo.acticles;
-DROP TABLE dbo.categories;
-DROP TABLE dbo.services;
-DROP TABLE dbo.priorities;
-DROP TABLE dbo.statuses;
-DROP DATABASE Knowledge;
